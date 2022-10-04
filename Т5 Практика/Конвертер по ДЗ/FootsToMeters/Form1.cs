8243a6b67ce417a0dca232f3d7556815890f90f9
SuperGTX76 <41799892+SuperGTX76@users.noreply.github.com>
2022-10-04 14:48:47 +0400

    Второй конвертер готов

--- Т5 Практика/Конвертер по ДЗ/FootsToMeters/Form1.cs
+++ Т5 Практика/Конвертер по ДЗ/FootsToMeters/Form1.cs
@@ -29,73 +29,106 @@ namespace FootsToMeters
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            AssignNum(button1);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            AssignNum(button2);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            AssignNum(button3);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+            AssignNum(button4);
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-
+            AssignNum(button5);
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-
+            AssignNum(button6);
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-
+            AssignNum(button7);
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-
+            AssignNum(button8);
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-
+            AssignNum(button9);
         }
 
         private void button0_Click(object sender, EventArgs e)
         {
+            if (_valueToConvert.Length <= 1)
+                return;
 
+            AssignNum(button0);
         }
 
         private void negativeButton_Click(object sender, EventArgs e)
         {
+            if (_valueToConvert.Length < 1)
+                return;
 
+            var value = Convert.ToDouble(_valueToConvert) * -1;
+            _valueToConvert = value.ToString();
+
+            UpgradeFootsLabel();
+            UpgradeMetersLabel();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            if (_valueToConvert.Length < 1)
+                return;
+
+            AssignZeroValue();
         }
 
         private void Comma_Click(object sender, EventArgs e)
         {
+            if (_valueToConvert.Length <= 1)
+                return;
+
+            if (_valueToConvert.Contains(_comma))
+                return;
+
+            AssignNum(Comma);
 
         }
 
         private void BackSpaceButton_Click(object sender, EventArgs e)
         {
+            if (_valueToConvert.Length <= 1)
+            {
+                AssignZeroValue();
+                return;
+            }
 
+                _valueToConvert = _valueToConvert.Remove(_valueToConvert.Length - 1, 1);
+
+                UpgradeFootsLabel();
+                UpgradeMetersLabel();
+            
         }
 
-        private void clearButton_Click(object sender, EventArgs e)
-        {
 
-        }
+        
 
         private void AssignNum(Button button)
         {
@@ -117,9 +150,20 @@ namespace FootsToMeters
         {
             var valueToConvert = Convert.ToDouble(_valueToConvert);
             var meters = valueToConvert * 0.30d;
-
-
-
+            string meterText2 = Convert.ToString(meters); //.Substring(0, MAX_STRING_LENGHT);
+            if (meterText2[meterText2.Length - 1] == _comma)
+            meterText2 = meterText2.Remove(meterText2.Length - 1, 1);
+            MetersLabel.Text = meterText2;
         }
+
+        private void AssignZeroValue()
+        {
+            _valueToConvert = String.Empty;
+            FootsLabel.Text = "0";
+            MetersLabel.Text = "0";
+        }
+
+        
+
     }
 }
