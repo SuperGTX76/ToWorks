e46adf9ac4a8b07085ddd8f2d910ed8e93bef2fa
SuperGTX76 <41799892+SuperGTX76@users.noreply.github.com>
2022-10-10 21:12:05 +0400

    Десятое задание

---
+++ Т10 практика/Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            
+            int[,,] array = new int[9, 9, 9];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {   
+                for (int k = 0; k < array.GetLength(2); k++)
+                    {
+                    array[i, j, k] = x;
+                    Console.WriteLine($"array[{i},{j},{k}] = {array[i, j, k]}");
+                    x++;
+                    if (x > 9) x = 1;
+                    
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
