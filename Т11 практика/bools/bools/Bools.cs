981949d41fc79030a4d59a4e775959cc126414af
SuperGTX76 <41799892+SuperGTX76@users.noreply.github.com>
2022-10-11 10:16:07 +0400

    T11

---
+++ Т11 практика/bools/bools/Bools.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bools
+{
+    class Bools
+    {
+        public static void Main (String[] arg)
+        {
+            Console.WriteLine("bool может принимать два выраения: false либо true.  Третьго не дано.");
+            Console.WriteLine("Значением по умолчанию для bool является false");
+            Console.WriteLine(" ");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Давайте лучше сыграем в угадайку? :)");
+            Console.WriteLine("Усли согласны - напишите 1, иначе 0");
+            Console.WriteLine("И не забудьте Энтер нажать!!!");
+
+            /* Здравствуйте.
+             * Можно было сделать это через свитч, но я решил делать эту часть кода на if конструкиях,
+             * используя для сравнения стринг, ибо невозможно предугадать, что введет особо опасный зверь с ником "ЮЗЕР" :)
+             */
+            string conditionSelect = Console.ReadLine();
+
+            if (conditionSelect != "0" && conditionSelect != "1")
+            {
+                Console.WriteLine("Чот ты мне тут ерунду всякую несешь! Правильные цифры надо тыкать - 0 или 1 !!!");
+                Console.WriteLine("Придется все по новой начинать!!!" );
+                return;
+            }
+                else if (conditionSelect == "0") 
+                {
+                    Console.WriteLine("Боязно, да? Ну на это я и рассчитывал! :)))");
+                    return;
+                }
+                else 
+                {
+                
+                }
+                
+
+
+            
+            Console.ReadLine();
+        }
+    }
+}
