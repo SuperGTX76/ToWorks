0e0f7b5eaa367c4bb9f458422191a79c814394e1
SuperGTX76 <41799892+SuperGTX76@users.noreply.github.com>
2022-10-13 22:05:41 +0400

    1

---
+++ Т16 практика/ConsoleAppExceptions2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppExceptions2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tankCapacity = 2000;                        //Общий объем воды в резервуаре
+            int consumptionWater = 500;                     //Сколько воды забрали
+            int replenishmentWater = -500;                  //Сколько воды пришло
+
+
+        }
+    }
+}
