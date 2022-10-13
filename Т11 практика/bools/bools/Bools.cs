ef27d6adc933245237896dd9797eaf8b70991135
SuperGTX76 <nalivayko2000@gmail.com>
2022-10-13 13:06:45 +0400

    Т12 выполнено

--- Т11 практика/bools/bools/Bools.cs
+++ Т11 практика/bools/bools/Bools.cs
@@ -12,7 +12,7 @@ namespace bools
         {
             Console.WriteLine("bool может принимать два выраения: false либо true.  Третьго не дано.");
             Console.WriteLine("Значением по умолчанию для bool является false");
-            Console.WriteLine(" ");
+            Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("Давайте лучше сыграем в угадайку? :)");
@@ -29,16 +29,64 @@ namespace bools
             {
                 Console.WriteLine("Чот ты мне тут ерунду всякую несешь! Правильные цифры надо тыкать - 0 или 1 !!!");
                 Console.WriteLine("Придется все по новой начинать!!!" );
+                Console.Read();
                 return;
             }
                 else if (conditionSelect == "0") 
                 {
                     Console.WriteLine("Боязно, да? Ну на это я и рассчитывал! :)))");
+                    Console.Read();
                     return;
                 }
+            
                 else 
                 {
+                Console.WriteLine("Да неужели в аптеки таблетки от трусости привезли???");
+                Console.WriteLine("Ну, коли шутки в стороны - то правила таковы:");
+                Console.WriteLine();
+                Console.WriteLine("Я загадываю число, от 0 до 10");
+                Console.WriteLine("Надо написать свое число, а я скажу, больше оно чем моё, или меньше.");
+                Console.WriteLine("Даю всего три попытки.");
+                Console.WriteLine();
+                Console.WriteLine("Итак, начинаем! Вводим первое число, и не забываем нажать Энтер.");
+
+                Random rnd = new Random();
+                int myValue = rnd.Next(0, 10);
+                        
                 
+
+                for (int i = 1; i < 4; i++)
+                {
+                    int playerValue = int.Parse(Console.ReadLine());
+                    if (myValue == playerValue)
+                    {
+                        Console.WriteLine($"Читер!!! Угадать с {i} попытки - это ЧИТЕРСТВО!"); 
+                    }
+                    else if (myValue < playerValue)
+                    {
+                        Console.WriteLine("Непрррраааавильно Вы, дядь Федор, бутерброд жуете!!!Я такие большие числа не ззагаааааадываю :)))");
+                        if (i == 3)
+                        {
+                            Console.WriteLine("Лууууузер! Попытки исчерпаны!");
+                            break;
+                        }
+                    }
+                    else if (myValue > playerValue)
+                    {
+                        Console.WriteLine("Мама Акула акуленку говорит - рот пошире разевай. Но моя цифра - ");
+                        Console.WriteLine("настолько велика, что даже туда не залезет... Ваша меньше!");
+                        if (i == 3)
+                        {
+                            Console.WriteLine("Лууууузер! Попытки исчерпаны!");
+                            break;
+                        }
+
+                    }
+                }
+                
+
+                        
+
                 }
                 
 
---
+++ Т12 практика/conditions/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание нумбер раз");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            int positiveNumber = 0;
+            int negativeNumber = 0;
+            int[] number = new int[5] { -20, 1, 51, -85, -9 };
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] > 0)
+                    positiveNumber++;
+                else if (number[i] < 0)
+                    negativeNumber++;
+
+                
+            }
+            Console.WriteLine($"Положительных  чисел {positiveNumber} а отрицательных {negativeNumber}" );
+
+
+            //Задание нумбер 2
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Задание нумбер 2");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("Числа Фиббоначи от 1 до 10 по порядку:");
+            int[] numberTwo = new int[10];
+            int summLastNumbers;
+            int lastNumber1 = 1;
+            int lastNumber2 = 0;
+            for (int i = 0; i < numberTwo.Length; i++)
+            {
+                summLastNumbers = lastNumber1 + lastNumber2;
+                numberTwo[i] = (summLastNumbers);
+                lastNumber1 = lastNumber2;
+                lastNumber2 = summLastNumbers;
+                Console.WriteLine(numberTwo[i]);
+
+            }
+
+
+
+            //Задание нумбер 3
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Задание нумбер 3");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("Сортировка массива в порядке возрастания:");
+            int[] number3 = new int[5] { 6, 31, 1, 45, 54 };
+
+
+            Array.Sort(number3);
+            Array.ForEach(number3, Console.WriteLine);
+
+            Console.ReadLine();
+        }
+    }
+}
