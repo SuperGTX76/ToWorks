7bbe4c78d16a2e3df3ec15ef62972a9a92c1392c
SuperGTX76 <41799892+SuperGTX76@users.noreply.github.com>
2022-10-16 17:20:47 +0400

    111

--- Т17 практика/ColorFontFile/Program.cs
+++ Т17 практика/ColorFontFile/Program.cs
@@ -11,7 +11,7 @@ namespace ColorFontFile
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\Сергей\Desktop\Синержи\СиШарп\ColorFontFileForTest";
+            string path = @"C:\123";
 
             if (Directory.Exists(path))
             {
@@ -21,7 +21,41 @@ namespace ColorFontFile
                 string[] dirs = Directory.GetDirectories(path);
                 foreach (string dir in dirs)
                     Console.WriteLine(dir);
+
+                Console.ResetColor();
+                
+                string[] filesName = Directory.GetFiles(path);
+                Console.WriteLine("Файлы: ");
+
+                foreach (string files in filesName)
+                {
+                    if (files.EndsWith(".zip"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(files);
+                    }  
+                    else if (files.EndsWith(".txt"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(files);
+                    }
+                    else if(files.EndsWith(".pptx"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.WriteLine(files);
+                    }
+
+                    Console.ResetColor();
+                }
+
+
             }
+
+            //Архивы красным,тхт зеленым, презенташки фиолетовым
+
+            
+
+
             
 
 
